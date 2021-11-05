1fd47259f8078e10e4c34041017a56f620685719
Toan <tvang994@gmail.com>
2021-11-04 17:40:38 -0700

    update UI

--- app/src/main/java/edu/neu/madcourse/stickittothem/SignupActivity.java
+++ app/src/main/java/edu/neu/madcourse/stickittothem/SignupActivity.java
@@ -1,13 +1,60 @@
 package edu.neu.madcourse.stickittothem;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+
 public class SignupActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+
+        Button registerButton = findViewById(R.id.register);
+//        registerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                registerUser()
+//            }
+//        });
+
+
+
+    }
+
+
+
+    private void registerUser(){
+        DatabaseReference myRef = FirebaseDatabase.getInstance().getReference("message");
+
+        Task t = myRef.setValue("Hello, World!");
+        if(!t.isSuccessful()){
+            Toast.makeText(getApplicationContext()
+                    , "Failed to write value into firebase. " , Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 }
